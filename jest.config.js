6876cc57d7e1ca2bce8c96fe969a8938096813a0
Richard McCartney <richard@mccartney.io>
2020-07-20 15:25:39 +0100

    Chore: includes emotion eslint config.

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
   ],
+  snapshotSerializers: ['jest-emotion']
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
